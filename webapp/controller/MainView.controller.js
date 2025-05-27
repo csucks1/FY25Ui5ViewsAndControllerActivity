70b8ea8fa358e72c1b817afed00895a57112194f
John Mac Millares <john.mac.m.millares@accenture.com>
2025-05-27 08:54:48 +0000

    route and navigation activity john.mac.m.millares

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -1,6 +1,6 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
 ], (Controller, MessageToast) => {
     "use strict";
     /**
@@ -52,13 +52,54 @@ sap.ui.define([
         },
         
         onPressCheckout: function (){
-            var oInputFNameValue = this.getView().byId("idInptFName").getValue();
-            var oInputLNameValue = this.getView().byId("idInptLName").getValue();
+            var oInputFName = this.getView().byId("idInptFName");
+            var oInputLName = this.getView().byId("idInptLName");
+            var oInputFNameValue = oInputFName.getValue();
+            var oInputLNameValue = oInputLName.getValue();
+            var oRouter = this.getOwnerComponent().getRouter();
 
-            // Check if first name is blank
-            if (oInputFNameValue === "" && oInputLNameValue === ""){
-                sap.m.MessageToast.show("Required Field is blank"); 
+            // Check if first name and last name is blank
+            if (oInputFNameValue === "" || oInputLNameValue === ""){
+               
+            // set value state to Error
+                oInputFName.setValueState("Error");
+                oInputLName.setValueState("Error");
+            } else {
+                oInputFName.setValueState("None");
+                oInputLName.setValueState("None");
+
+                //Navigate to review page passing first
+                oRouter.navTo("RouteReviewPage", { firstName: oInputFNameValue });
+
             }
-        }
+        },
+
+        
+
+        onAddItem: function (){
+            // Comment this code for now
+            // var oTextBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+            // var sMsg = oTextBundle.getText("addButtonMsg");
+            // this.fnDisplayMsg(sMsg);
+
+            // Instantiate the fragment
+            // create dialog lazily
+            if (!this.oDialog) {
+                // By using loadFragment, we are adding the fragment as a dependent to the View
+                // By doing so, we can use the functions inside the view's controller
+                this.oDialog = this.loadFragment({
+                    name: "com.training.exer1millares.fragment.ProductDialog"
+                });
+            } 
+            this.oDialog.then(function(oDialog) {
+                oDialog.open();
+            });
+        },
+
+        onCloseDialog: function (){
+            this.getView().byId("idProductDialog").close();
+        },
+
+
     });
 });
